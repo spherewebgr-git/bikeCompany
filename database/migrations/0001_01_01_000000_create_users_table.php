c8f31cb00a1315c6c7f073b39b12ed73b171c223
Kostis Verikakis <s.karagiannis@egov.demokritos.gr>
2026-07-15 11:06:36 +0300

    user database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->unique();
             $table->string('email')->unique();
+            $table->string('card_number')->unique();
+            $table->string('card_exp_month');
+            $table->string('card_exp_year');
+            $table->string('card_cvv');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('role_id')->constrained('roles');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
+use App\Models\Provision;
+use App\Models\Role;
+use App\Models\Speed;
+use App\Models\Type;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -24,11 +31,25 @@ class DatabaseSeeder extends Seeder
             ProvisionSeeder::class,
             BikeSeeder::class,
             PriceSeeder::class,
+            RoleSeeder::class,
         ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $users = json_decode(file_get_contents(database_path('data/USERS.json')), true);
+
+        foreach ($users as $user) {
+            DB::table('users')->insert([
+                'first_name' => $user['first_name'],
+                'last_name' => $user['last_name'],
+                'email' => $user['email'],
+                'phone' => $user['phone'],
+                'password' => Hash::make($user['password']),
+                'card_number' => $user['card_number'],
+                'card_exp_month' => $user['card_exp_month'],
+                'card_exp_year' => $user['card_exp_year'],
+                'card_cvv' => $user['card_cvv'],
+                'role_id' => Role::inRandomOrder()->value('id'),
+
+            ]);
+        }
     }
 }
