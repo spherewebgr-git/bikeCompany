9dce278861089469c13e035d06ab3f2ff549e89d
kostisver <kostisverikakis13@gmail.com>
2026-07-31 15:43:35 +0300

    contact-us profile app scss

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -107,21 +107,21 @@
 
     <script>
         document.querySelectorAll('.toggle-password').forEach(icon => {
-
             icon.addEventListener('click', function () {
-
                 const input = document.getElementById(this.dataset.target);
 
                 if (input.type === 'password') {
                     input.type = 'text';
 
+                    this.classList.remove('fa-eye');
+                    this.classList.add('fa-eye-slash');
                 } else {
                     input.type = 'password';
 
+                    this.classList.remove('fa-eye-slash');
+                    this.classList.add('fa-eye');
                 }
-
             });
-
         });
     </script>
 </section>
