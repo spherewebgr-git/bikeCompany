c9863c61d18cd3dc767b988cbc3b8d2d493400f5
Senri <senri9402@gmail.com>
2026-08-28 12:17:23 +0300

    typos

--- resources/js/react/components/Sidebar.jsx
+++ resources/js/react/components/Sidebar.jsx
@@ -60,10 +60,10 @@ export default function Sidebar()
                     </Link>
                 </li>
 
-                <li class="bold">
-                    <Link class="waves-effect waves-cyan" to="/admin/manage/reviews">
-                        <i class="fa-solid fa-star-half-stroke"></i>
-                        <span class="menu-title">Reviews Management</span>
+                <li className="bold">
+                    <Link className="waves-effect waves-cyan" to="/admin/manage/reviews">
+                        <i className="fa-solid fa-star-half-stroke"></i>
+                        <span className="menu-title">Reviews Management</span>
                     </Link>
                 </li>
 
